41cdf9a19034878a8fcc323dd418511cff449390
TiagoSemedoVaz <37613@esjaloures.org>
2026-02-11 11:06:08 +0000

    S1.

--- src/main/java/com/example/Biblioteca.java
+++ src/main/java/com/example/Biblioteca.java
@@ -1,4 +1,4 @@
-package  com.example;
+package com.example;
 
 import java.util.ArrayList;
 
@@ -12,13 +12,51 @@ public class Biblioteca {
 
     public boolean adicionarLivro(Livro livro) {
         
+       boolean aTitulo = true;
+    
+       for(Livro livro2 : livros)
+       {
+            if(livro2.getTitulo() == livro.getTitulo())
+            {
+                
+                aTitulo = false;
+                
+                break;
+            }
+       }
+       
+       if(aTitulo)
+       {
+           livros.add(livro);
+       }
+       
+       return aTitulo;
     }
 
     public boolean removerLivro(String titulo) {
         
+        for(Livro livro:livros)
+        {
+            if(livro.getTitulo() == titulo)
+            {
+                livros.remove(livro);
+                
+                return true;
+            }
+        }
+        
+        return false;
     }
 
     public void listarLivros() {
+        
+        for(Livro livro:livros)
+        {
+             
+            System.out.println("titulo: " + livro.getTitulo() + "\n" + "Autor: " + livro.getAutor() + "\n"
+            + "Ano: " + livro.getAno() + "\n");
+            
+        }
         
     }
 }
